16c25bcdcc72d679db7a3f465c066eccf2a2b788
tiwariParth <243mjparth@gmail.com>
2024-08-25 15:06:06 +0530

    chore: Update dependencies and implement payment processing logic in bank-webhook app

--- apps/bank-webhook/src/index.ts
+++ apps/bank-webhook/src/index.ts
@@ -1,13 +1,35 @@
 import express, { Request, Response } from "express";
+import db from "@repo/db/client";
 
 const app = express();
 
-app.post("/hdfcWebHook", (req: Request, res: Response) => {
+app.post("/hdfcWebHook", async (req: Request, res: Response) => {
   const paymentOptions = {
     token: req.body.token,
     amount: req.body.amount,
     userId: req.body.user_identifier,
   };
+
+  await db.balance.update({
+    where: {
+      userId: paymentOptions.userId,
+    },
+    data: {
+      amount: {
+        increment: paymentOptions.amount,
+      },
+    },
+  });
+
+  await db.onRampTransaction.update({
+    where: {
+      token: paymentOptions.token,
+    },
+    data: {
+      status: "Success",
+    },
+  });
+  res.status(200).json({ message: "Payment successful" });
 });
 
 app.listen(3000, () => {
