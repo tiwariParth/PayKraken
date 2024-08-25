5e4824d964042484c0a7119705ee155d22a78e0f
tiwariParth <243mjparth@gmail.com>
2024-08-25 22:06:30 +0530

    chore: Implement HDFC webhook payment processing

--- apps/bank-webhook/src/index.ts
+++ apps/bank-webhook/src/index.ts
@@ -1,37 +1,54 @@
-import express, { Request, Response } from "express";
+import express from "express";
 import db from "@repo/db/client";
-
 const app = express();
 
-app.post("/hdfcWebHook", async (req: Request, res: Response) => {
-  const paymentOptions = {
+app.use(express.json());
+
+app.post("/hdfcWebhook", async (req, res) => {
+  //TODO: Add zod validation here?
+  //TODO: HDFC bank should ideally send us a secret so we know this is sent by them
+  const paymentInformation: {
+    token: string;
+    userId: string;
+    amount: string;
+  } = {
     token: req.body.token,
-    amount: req.body.amount,
     userId: req.body.user_identifier,
+    amount: req.body.amount,
   };
 
-  await db.balance.update({
-    where: {
-      userId: paymentOptions.userId,
-    },
-    data: {
-      amount: {
-        increment: paymentOptions.amount,
-      },
-    },
-  });
+  try {
+    await db.$transaction([
+      db.balance.updateMany({
+        where: {
+          userId: Number(paymentInformation.userId),
+        },
+        data: {
+          amount: {
+            // You can also get this from your DB
+            increment: Number(paymentInformation.amount),
+          },
+        },
+      }),
+      db.onRampTransaction.updateMany({
+        where: {
+          token: paymentInformation.token,
+        },
+        data: {
+          status: "Success",
+        },
+      }),
+    ]);
 
-  await db.onRampTransaction.update({
-    where: {
-      token: paymentOptions.token,
-    },
-    data: {
-      status: "Success",
-    },
-  });
-  res.status(200).json({ message: "Payment successful" });
+    res.json({
+      message: "Captured",
+    });
+  } catch (e) {
+    console.error(e);
+    res.status(411).json({
+      message: "Error while processing webhook",
+    });
+  }
 });
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
-});
+app.listen(3003);
